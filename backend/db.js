1294b916dfbf34570d364eea7e9773b5ae857a24
Hridyansh173 <sridh429@gmail.com>
2024-06-24 10:50:05 +0530

    successful connection with mongoose db

---
+++ backend/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const mongoURI = "mongodb://localhost:27017/";
+
+const connectToMongo = async () => {
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("Connection successful");
+  } catch (error) {
+    console.error("Error connecting to MongoDB", error);
+  }
+};
+
+module.exports = connectToMongo;
